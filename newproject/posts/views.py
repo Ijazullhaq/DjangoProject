71b0907c4019b8a4ea38ab92a8f0107eae7a6433
ijazullhaq <ijazullhaq400@gmail.com>
2025-02-21 12:19:44 +0430

    updated

--- newproject/posts/views.py
+++ newproject/posts/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        "id":1,
+        "title":'Let Explore Python',
+        "content": 'Python is interpreted, high-level language'
+    }
+]
 
-def helloWorld(request):
-    return HttpResponse("Hello World?")
+def home(request):
+    html = ""
+    for post in posts:
+        html += f'''
+            <div>
+                <h1>{post['id']} - {post['title']}</h1>
+                <p>{post['content']}</p>
+            </div>
+'''
+    return HttpResponse(html)
+# dynamic url id 
+def post (request, id):
+    return HttpResponse(id)
 
